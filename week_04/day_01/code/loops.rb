95825250ad191e31d357a7e03a5561169c974849
ga-wolf <jack.jeffress@ga.co>
2018-10-21 14:56:17 +0300

    Added loop stuff

---
+++ week_04/day_01/code/loops.rb
@@ -0,0 +1,25 @@
+i = 1
+
+while i <= 5
+  p "I: #{i}"
+  i += 1
+end
+
+# Create a loop that goes from 5 down to 1
+count = 5
+
+while count > 0
+  p "Count: #{count}"
+  count -= 1
+end
+
+index = 1
+
+until index == 6
+  p "Index: #{index}"
+  index += 1
+end
+
+for idx in 0..5
+ p "Idx: #{idx}"
+end
