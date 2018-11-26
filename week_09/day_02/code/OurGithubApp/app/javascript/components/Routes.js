7a60ecc8decba30188ea20d641069514dbad1532
ga-wolf <jack.jeffress@ga.co>
2018-11-26 16:46:08 +0300

    About to add User stuff

--- week_09/day_02/code/OurGithubApp/app/javascript/components/Routes.js
+++ week_09/day_02/code/OurGithubApp/app/javascript/components/Routes.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { HashRouter, Route, Link } from "react-router-dom";
+import { AnimatedSwitch } from "react-router-transition";
+
 import Home from "./Home";
 import Search from "./Search";
 import UserShow from "./UserShow";
@@ -14,9 +16,17 @@ class Routes extends React.Component {
             <Link to="/search">Search</Link>
             <Link to="/users/mfalthaw">Moath's Profile</Link>
           </nav>
-          <Route exact path="/" component={Home} />
-          <Route path="/search" component={Search} />
-          <Route path="/users/:username" component={UserShow} />
+
+          <AnimatedSwitch
+            atEnter={{ opacity: 0 }}
+            atLeave={{ opacity: 0 }}
+            atActive={{ opacity: 1 }}
+            className="switch-wrapper"
+          >
+            <Route exact path="/" component={Home} />
+            <Route path="/search" component={Search} />
+            <Route path="/users/:username" component={UserShow} />
+          </AnimatedSwitch>
         </div>
       </HashRouter>
     );
